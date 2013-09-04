caa8366783bffb3b738cf6174644f4285fca8ecb
Steve Phillips <elimisteve@gmail.com>
2013-09-04 07:19:13 -0700

    Block definition: 'inputs' and 'outputs' are now arrays

--- web.go
+++ web.go
@@ -107,15 +107,19 @@ var BLOCK_DEFINITION = []byte(`
   "name": "HTTP Status Code Retriever",
   "url": "http://status-code-retriever.herokuapp.com",
   "description": "Visits the given URL and returns the HTTP status code.",
-  "inputs": {
+  "inputs": [
+    {
       "name": "url",
       "type": "String",
       "description": "URL to be visited."
-  },
-  "outputs": {
+    }
+  ],
+  "outputs": [
+    {
       "name": "status_code",
       "type": "Number",
       "description": "HTTP status code returned by given URL."
-  }
+    }
+  ]
 }
 `)
